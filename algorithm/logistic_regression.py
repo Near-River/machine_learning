f8f23a5705e3bb787cb1209aa0b4f0feee294999
Near_River <798563420@qq.com>
2016-06-08 18:44:34 +0800

    d_006 logistic regression algorithm

--- algorithm/logistic_regression.py
+++ algorithm/logistic_regression.py
@@ -2,44 +2,70 @@
 # coding=utf-8
 
 import numpy as np
-import numpy.linalg as lg
 import matplotlib.pyplot as plt
+from math import exp
 
 """
 Logistic Regression Algorithm
 """
 
 
-def plot_data(dataset, W_Lin):
-    X = dataset[:, 0:-1]
+def plot_data(dataset, W):
     Y = np.array(dataset[:, -1], dtype=int)
+    X_pos = (dataset[Y == 0])[:, 0:-1]
+    X_neg = (dataset[Y == 1])[:, 0:-1]
 
     plt.figure()
     # Plot the points
-    plt.plot(X, Y, '*')
-    # Plot the logistic regression line
-    X_Lin = np.linspace(0, 25, 1000)[:, np.newaxis]
-    Y_Lin = np.dot(np.hstack((np.ones((1000, 1)), X_Lin)), W_Lin)
-    plt.plot(X_Lin, Y_Lin)
+    plt.plot(X_pos[:, 0], X_pos[:, 1], 'o')
+    plt.plot(X_neg[:, 0], X_neg[:, 1], 'x')
+    # Plot the logistic devision line
+
     plt.show()
+
+
+def sigmoid(Z):
+    if type(Z) == float or type(Z) == int:
+        return 1 / (1 + exp(-1 * Z))
+    if type(Z) == np.ndarray:
+        return 1 / (1 + np.exp(-1 * Z))
 
 
 def logistic_regression_learning(dataset):
     X = dataset[:, 0:-1]
     rows, cols = X.shape
     X = np.hstack((np.ones((rows, 1)), X))
-    Y = np.array(dataset[:, -1], dtype=int)
+    Y = np.array(dataset[:, -1], dtype=int)[:, np.newaxis]
+    Y[Y == 0] = -1
 
-    W = np.zeros((1, cols))[0]
-    X, Y = np.mat(X), np.mat(Y).transpose()
+    # Initial W (Wo = 0)
+    W = np.zeros((cols + 1, 1))
+    # Definite the fixed learning rate
+    theta = 0.001
+    N = rows
+    count, loop_times = 0, 400
 
-    print(X.shape, Y.shape)
-    print(W.shape)
+    while count < loop_times:
+        # Derviation the derviative of In-sample-error
+        derviate = (1 / N) * np.sum(sigmoid(-1 * Y * X.dot(W)) * (-1 * Y * X), axis=0)
+        if (derviate == np.zeros((cols + 1,))).all():
+            break
+        # Update the W
+        W -= theta * derviate[:, np.newaxis]
+        count += 1
+
+    # Statistic accuracy of the problems
+    res = np.dot(X, W)
+    # print(res)
+    positive_true = np.count_nonzero(Y[res >= 0.5] == 1)
+    negitive_false = np.count_nonzero(Y[res < 0.5] == -1)
+    print('Accuracy: %d' % (positive_true + negitive_false))
 
     return W
 
 
 if __name__ == '__main__':
     dataset = np.genfromtxt('logistic_regression_data.txt', delimiter=',')
-    W_Lin = logistic_regression_learning(dataset)
-    # plot_data(dataset, W_Lin)
+    W = logistic_regression_learning(dataset)
+    print(W)
+    # plot_data(dataset, W)
