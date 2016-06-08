9276fc13b53ec7dbf46e809c12a89fafaaa54f73
Near_River <798563420@qq.com>
2016-06-08 15:54:41 +0800

    d_005 linear regression algorithm

---
+++ algorithm/linear_regression.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import numpy as np
+import numpy.linalg as lg
+import matplotlib.pyplot as plt
+
+"""
+Linear Regression Algorithm
+"""
+
+
+def plot_data(dataset, W_Lin):
+    X = dataset[:, 0:-1]
+    Y = np.array(dataset[:, -1], dtype=int)
+
+    plt.figure()
+    # Plot the points
+    plt.plot(X, Y, '*')
+    # Plot the linear regression line
+    X_Lin = np.linspace(0, 25, 1000)[:, np.newaxis]
+    Y_Lin = np.dot(np.hstack((np.ones((1000, 1)), X_Lin)), W_Lin)
+    plt.plot(X_Lin, Y_Lin)
+    plt.show()
+
+
+def linear_regression_learning(dataset):
+    X = dataset[:, 0:-1]
+    rows, cols = X.shape
+    X = np.hstack((np.ones((rows, 1)), X))
+    Y = dataset[:, -1]
+    W = np.zeros((1, cols))[0]
+
+    X, Y = np.mat(X), np.mat(Y).transpose()
+    W = lg.inv(X.transpose() * X) * X.transpose() * Y
+    return W
+
+
+if __name__ == '__main__':
+    dataset = np.genfromtxt('linear_regression_data.txt', delimiter=',')
+    W_Lin = linear_regression_learning(dataset)
+    plot_data(dataset, W_Lin)
